5f466349ae82d828a908f756b87388b03b67c57d
Brucegx <guoxi.bruce@gmail.com>
2016-08-12 16:39:21 -0400

    8.12

--- employees/client/components/employee_list.js
+++ employees/client/components/employee_list.js
@@ -1,26 +1,43 @@
-import React from 'react';
+
+import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
+const PER_PAGE = 21;
 
-
-const EmployeeList = (props) => {
+class EmployeeList extends Component {
+	componentWillMount(){
+		this.page = 1;
+	}
+	handleButtonClick(){
+		Meteor.subscribe('employees', PER_PAGE * (this.page + 1));
+		this.page += 1;
+	}
+	render() {
 	// props.employees => an array of employee objects
-	console.log(props);
+	//the difference between props.employee and props is that props is the object
+	//which contains employee's array and other things like other configuration.
 	return (
 		<div>
 			<div className="employee-list">
-				{props.employees.map(employee => <EmployeeDetail employee={employee} />)}
+				{this.props.employees.map(employee => 
+					<EmployeeDetail key={employee._id} employee={employee} />
+					)}
 			</div>
+			<button onClick={this.handleButtonClick.bind(this) }
+				className="btn btn-primary">
+				Load More
+			</button>
 		</div>
-	);
+		);
+	}
 }; 
 
 
 export default createContainer(() => {
 	//set up subscription
-	Meteor.subscribe('employees');
+	Meteor.subscribe('employees', PER_PAGE);
 
 	//return an object. Whatever we return will be sent to EmployeeList as props
 
